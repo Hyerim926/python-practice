f80bfea6226f84f0ad69a18807cb6020bab77e4f
Hyerim926 <ekfrhsk96@icloud.com>
2024-03-14 02:41:08 +0900

    feat: print func

---
+++ print/print1.py
@@ -0,0 +1,20 @@
+# Print 사용법
+# /python3_formatted_output.php
+
+# 기본 출력
+print('Python Start!')
+print('Python Start!')
+print("Python Start!")
+print('''Python Start!''')
+print("""Python Start!""")
+
+print()
+
+# seperator 옵션 (js의 Array.join()과 비슷)
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '7777', '7777', sep='-')
+print('python', 'gmail.com', sep='@')
+
+print()
+
+# end 옵션

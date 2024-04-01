d01e54ebc214bce03bca968865bee741b9f3e430
Hyerim926 <ekfrhsk96@icloud.com>
2024-04-01 22:28:31 +0900

    feat: package

---
+++ package/package.py
@@ -0,0 +1,48 @@
+# 패키지
+# 패키지 작성 및 사용법
+# 파이썬은 패키지로 분할된 개별적인 모듈로 구성
+# __init__.py : Python 3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성을 추천한다, 안그러면 import가 되지 않음 / 약간 js에서 export의 느낌
+# 상대경로 : ..(부모 디렉토리), .(현재 디렉토리) -> 모듈 내부에서만 사용
+
+# 예제 1 (같은 경로에 있는 패키지)
+import sub.sub1.module1
+import sub.sub2.module2
+
+# 사용
+sub.sub1.module1.mod1_test1()
+sub.sub1.module1.mod1_test2()
+
+sub.sub2.module2.mod2_test1()
+sub.sub2.module2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제 2
+# js ver: import * from sub.sub2
+from sub.sub1 import module1
+from sub.sub2 import module2 as m2  # alias
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+m2.mod2_test1()
+m2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제 3
+from sub.sub1 import *  # 모든 메서드를 가지고 오는 방법, 하지만 지양하자
+from sub.sub2 import *
+
+module1.mod1_test1()
+module1.mod1_test2()
+module2.mod2_test1()
+module2.mod2_test2()
+
+print()
+print()
+print()

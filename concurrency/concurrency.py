4d62bdf3b024b2115f78577bd4a91bf27e507ccc
Hyerim926 <ekfrhsk96@icloud.com>
2024-04-29 23:14:26 +0900

    feat: generator pattern

--- concurrency/concurrency.py
+++ concurrency/concurrency.py
@@ -42,3 +42,68 @@ from collections import abc
 
 print(hasattr(t, '__iter__'))  # True
 print(isinstance(t, abc.Iterable))  # True
+
+
+# next Pattern
+class WordSpliter:
+    def __init__(self, text):
+        self._idx = 0
+        self._text = text.split(' ')
+
+    def __next__(self):
+        print('Called __next__')
+        try:
+            word = self._text[self._idx]
+        except IndexError:
+            raise StopIteration('Stopped Iteration.')
+        self._idx += 1
+        return word
+
+    def __repr__(self):
+        return 'WordSplit(%s)' % self._text
+
+
+wi = WordSpliter('Do today what you could do tomorrow')
+print(wi)
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+# print(next(wi))
+
+print()
+print()
+
+
+# Generator Pattern
+# 1. 지능형 리스트, 딕셔너리, 집합 -> 데이터 양 증가 후 메모리 사용량 증가 -> 제네레이터 사용 권장
+# 2. 단위 실행 가능한 코루틴(Coroutine) 구현과 연동
+# 3. 작은 메모리 조각 사용
+
+class WordSplitGenerator:
+    def __init__(self, text):
+        self._text = text.split(' ')
+
+    def __iter__(self):
+        for word in self._text:
+            yield word  # 제네레이터
+        return
+
+    def __repr__(self):
+        return 'WordSplitGenerator(%s)' % self._text
+
+
+wg = WordSplitGenerator('Do today what you could do tomorrow')
+
+wt = iter(wg)
+
+print(wt, wg)
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+# print(next(wt))
